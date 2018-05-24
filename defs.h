0ea0f06439b70bb741303d8aec0ea6a3b901e4d4
nishamanthala <nmant001@ucr.edu>
2018-05-23 17:23:14 -0700

    updated

--- defs.h
+++ defs.h
@@ -104,7 +104,7 @@ int             pipewrite(struct pipe*, char*, int);
 //PAGEBREAK: 16
 // proc.c
 int             cpuid(void);
-void            exit(void);
+void            exit(int status);
 int             fork(void);
 int             growproc(int);
 int             kill(int);
@@ -117,9 +117,11 @@ void            sched(void);
 void            setproc(struct proc*);
 void            sleep(void*, struct spinlock*);
 void            userinit(void);
-int             wait(void);
+int             wait(int*);
+int             waitpid(int pid, int *status, int options);
 void            wakeup(void*);
 void            yield(void);
+void            setPriority(int priority); // added setPriority function 
 
 // swtch.S
 void            swtch(struct context**, struct context*);
