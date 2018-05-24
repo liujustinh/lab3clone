d19d6ca2122d6e4e098751624c3508041e5f54c4
nishamanthala <nmant001@ucr.edu>
2018-05-23 17:00:57 -0700

    updated

---
+++ lab2_test.c
@@ -0,0 +1,14 @@
+#include "types.h" 
+#include "user.h" 
+#include "stat.h" 
+#include "fcntl.h" 
+
+int main(int argc, char *argv[]) {		//lab2_test [amount of child processes] [ 
+	if (argc < 2) {
+		displayProcesses(); 
+		exit(0); 
+	}
+}
+
+void displayProcesses()
+{ }
