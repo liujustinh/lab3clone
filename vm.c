6b8051d20ecb37d9f3a95e2e4477e40bdf1b1b11
nishamanthala <nmant001@ucr.edu>
2018-05-23 21:08:19 -0700

    updated3

--- vm.c
+++ vm.c
@@ -335,7 +335,20 @@ copyuvm(pde_t *pgdir, uint sz)
     if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
       goto bad;
   }
-  return d;
+  for (i = KERNBASE - spgs*PGSIZE; i < KERNBASE && spgs > 0; i += PGSIZE) {
+    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+      panic("copyuvm: pte should exist");
+    if(!(*pte & PTE_P))
+      panic("copyuvm: page not present");
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memmove(mem, (char*)P2V(pa), PGSIZE);
+    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
+      goto bad;
+    --spgs;
+  }
 
 bad:
   freevm(d);
