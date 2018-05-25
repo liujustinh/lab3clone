e4d4fd8ddc6256fbe277e718c803f34d6e9114f9
Justin <jliu076@ucr.edu>
2018-05-25 08:29:59 +0000

    fixed sysproc

--- vm.c
+++ vm.c
@@ -322,6 +322,19 @@ copyuvm(pde_t *pgdir, uint sz, uint stack_sz)
 
   if((d = setupkvm()) == 0)
     return 0;
+  for(i = 0; i < sz; i += PGSIZE){
+    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+      panic("copyuvm: pte should exist");
+    if(!(*pte & PTE_P))
+      panic("copyuvm: page not present 1");
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memmove(mem, (char*)P2V(pa), PGSIZE);
+    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
+      goto bad;
+  }
   for(i = KERNBASE - stack_sz*PGSIZE; i < KERNBASE && stack_sz > 0; i += PGSIZE){
     if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
       panic("copyuvm: pte should exist");
@@ -334,7 +347,7 @@ copyuvm(pde_t *pgdir, uint sz, uint stack_sz)
     memmove(mem, (char*)P2V(pa), PGSIZE);
     if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0)
       goto bad;
-      stack_sz--; 
+      --stack_sz; 
   }
   return d;
 
