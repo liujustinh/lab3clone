23f6487bbce4309f00f32f982ec1ab49637a772c
Justin <jliu076@ucr.edu>
2018-05-25 05:15:59 +0000

    added holder for total pages in stack

--- proc.h
+++ proc.h
@@ -37,6 +37,7 @@ enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 // Per-process state
 struct proc {
   uint sz;                     // Size of process memory (bytes)
+  uint stack_sz; 		// Holds total number of pages in stack 
   pde_t* pgdir;                // Page table
   char *kstack;                // Bottom of kernel stack for this process
   enum procstate state;        // Process state
